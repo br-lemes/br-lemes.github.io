d6e73e997bd7507234b393b5a26eaa017c74cdbe
Breno Ramalho Lemes <breno@br-lemes.net>
2014-11-08 22:19:30 -0300

    Removido os traços do navbar-icon, não mais utilizado.

--- js/theme.js
+++
@@ -1,9 +0,0 @@
-$("i.navbar-icon").next().css("display", "none")
-$("i.navbar-icon").hover(
-	function(){
-		$(this).next().css("display", "initial")
-	},
-	function(){
-		$(this).next().css("display", "none")
-	}
-)
